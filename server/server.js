bd0a882353eb446c86de65766448bb146a02b61f
Komal Singh <komsingh@WKMZT7C085F3.global.publicisgroupe.net>
2021-08-05 09:49:58 +0530

    Update Dependecies

--- server/server.js
+++ server/server.js
@@ -10,30 +10,43 @@ const db = require('./db');
 const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
-const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
 
-const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf-8'}));
+async function startApolloServer() {
+  // Construct a schema, using GraphQL schema language
+  const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf-8' }));
 
-const resolvers = require('./resolver');
+  // Provide resolver functions for your schema fields
+  const resolvers = require('./resolver');
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers, context: ({req}) => ({
-  user: req.user
-})});
-apolloServer.applyMiddleware({ app, path: '/graphql' });
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  const user = db.users.list().find((user) => user.email === email);
-  if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
-  }
-  const token = jwt.sign({ sub: user.id }, jwtSecret);
-  res.send({ token });
-});
+  const server = new ApolloServer({ typeDefs, resolvers,  context: ({ req }) => ({
+    user: req.user
+  }) });
+  await server.start();
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+  const app = express();
+  app.use(cors(), express.json(), expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false, algorithms: ['HS256']
+  }));
+  
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+
+  app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = db.users.list().find((user) => user.email === email);
+    if (!(user && user.password === password)) {
+      res.sendStatus(401);
+      return;
+    }
+    const token = jwt.sign({ sub: user.id }, jwtSecret);
+    res.send({ token });
+  });
+
+  return { server, app };
+}
+
+startApolloServer()
